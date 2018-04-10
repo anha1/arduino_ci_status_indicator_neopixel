0276fed28526a48d5c35a1cb8ce8755e4e97daa0
anha1 <anton.haidai@gmail.com>
2018-04-10 20:27:56 +0000

    Midori script, install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+dir=/usr/local/ci-status-neopixel
+if [ "$PWD" != "$dir" ]; then
+	echo "Please run from '$dir'!";
+	exit 1;
+fi
+cp -n ./systemd/ci-midori.service /usr/lib/systemd/system/ci-midori.service
+cp -n ./systemd/ci-status-neopixel.service /usr/lib/systemd/system/ci-status-neopixel.service
+cp -n ./service/settings.ini /usr/local/ci-status-neopixel/settings.ini
+systemctl daemon-reload
+systemctl enable ci-status-neopixel
+systemctl enable ci-midori
+systemctl start ci-status-neopixel
+systemctl start ci-midori
+echo "done!"
---
+++ midori/ci-midori.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+midori -a http://localhost:8099 --display=:0.0 -i 30 -e Fullscreen
